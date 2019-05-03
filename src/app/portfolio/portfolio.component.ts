04ca21051c2ed91232de677f41b4842fc4fbea42
Vince Carpino <vcarpino33@gmail.com>
2019-05-03 13:31:36 -0700

    Adds basis of portfolio image grid

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,11 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  url: string;
+}
+
 @Component({
   templateUrl: './portfolio.component.html',
   styleUrls: ['./portfolio.component.scss']
 })
 export class PortfolioComponent implements OnInit {
   pageTitle = 'Portfolio';
+
+  tiles: Tile[] = [
+    {url: '/src/assets/photos/horiz.jpeg', cols: 2, rows: 1, color: 'lightblue'},
+    {url: '/src/assets/photos/vert.jpeg', cols: 1, rows: 2, color: 'lightblue'},
+    {url: '/src/assets/photos/horiz.jpeg', cols: 2, rows: 1, color: 'lightblue'},
+    {url: '/src/assets/photos/vert.jpeg', cols: 1, rows: 2, color: 'lightblue'},
+    {url: '/src/assets/photos/horiz.jpeg', cols: 2, rows: 1, color: 'lightblue'},
+  ];
+  tiles2: Tile[] = [
+    {url: '/src/assets/photos/vert.jpeg', cols: 1, rows: 2, color: 'lightblue'},
+    {url: '/src/assets/photos/vert.jpeg', cols: 1, rows: 2, color: 'lightblue'},
+    {url: '/src/assets/photos/horiz.jpeg', cols: 2, rows: 1, color: 'lightblue'},
+    {url: '/src/assets/photos/horiz.jpeg', cols: 2, rows: 1, color: 'lightblue'},
+    {url: '/src/assets/photos/vert.jpeg', cols: 1, rows: 2, color: 'lightblue'},
+  ];
+  tiles3: Tile[] = [
+    {url: '/src/assets/photos/horiz.jpeg', cols: 2, rows: 1, color: 'lightblue'},
+    {url: '/src/assets/photos/vert.jpeg', cols: 1, rows: 2, color: 'lightblue'},
+    {url: '/src/assets/photos/horiz.jpeg', cols: 2, rows: 1, color: 'lightblue'},
+    {url: '/src/assets/photos/vert.jpeg', cols: 1, rows: 2, color: 'lightblue'},
+    {url: '/src/assets/photos/horiz.jpeg', cols: 2, rows: 1, color: 'lightblue'},
+  ];
 
   constructor() {}
 
