abc9db882a4ad47b95bf00b4e11f8d4c2689cb7d
Vince Carpino <vcarpino33@gmail.com>
2019-05-10 15:50:54 -0700

    Adds contact form and styling

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './contact.component.html',
@@ -6,6 +7,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
   pageTitle = 'Contact';
+
+  name = new FormControl('', [Validators.required]);
+  email = new FormControl('', [Validators.required, Validators.email]);
+  message = new FormControl('', [Validators.required]);
+
+  nameString = '';
+  emailString = '';
+  messageString = '';
+
+  getErrorMessage(field: string) {
+    switch (field) {
+      case 'name':
+        return this.name.hasError('required') ? 'Please enter a value' : '';
+      case 'email':
+        return this.email.hasError('required')
+          ? 'Please enter a value'
+          : this.email.hasError('email')
+          ? 'Invalid email'
+          : '';
+      case 'message':
+        return this.message.hasError('required') ? 'Please enter a value' : '';
+      default:
+        break;
+    }
+  }
+
+  /**
+   * Process the form we have. Send to whatever backend
+   * Only alerting for now
+   */
+  processForm() {
+    const allInfo = `My name is ${this.nameString}. My email is ${
+      this.emailString
+    }. My message is ${this.messageString}`;
+
+    alert(allInfo);
+  }
+
+  anyErrors(): boolean {
+    return (
+      this.name.hasError('required') ||
+      this.email.hasError('required') ||
+      this.email.hasError('email') ||
+      this.message.hasError('required')
+    );
+  }
 
   constructor() {}
 
