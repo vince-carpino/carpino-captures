c5c3f0f820aedfe19634ff03a11382e7a55a611e
Vince Carpino <vcarpino33@gmail.com>
2019-06-04 18:45:53 -0700

    Adds working S3 links for portfolio images

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,56 +8,77 @@ import { Component, OnInit } from '@angular/core';
 export class PortfolioComponent implements OnInit {
   pageTitle = 'Portfolio';
 
-  imageUrls: string[] = [
-    // 'https://s3-us-west-1.amazonaws.com/carpino-captures/01.JPG',
-    // 'https://s3-us-west-1.amazonaws.com/carpino-captures/02.JPG',
-    // 'https://s3-us-west-1.amazonaws.com/carpino-captures/03.JPG',
-    // 'https://s3-us-west-1.amazonaws.com/carpino-captures/04.JPG',
-    // 'https://s3-us-west-1.amazonaws.com/carpino-captures/05.JPG',
-    // 'https://s3-us-west-1.amazonaws.com/carpino-captures/06.JPG',
-    // 'https://s3-us-west-1.amazonaws.com/carpino-captures/07.JPG',
-    // 'https://s3-us-west-1.amazonaws.com/carpino-captures/08.JPG',
-    // 'https://s3-us-west-1.amazonaws.com/carpino-captures/09.JPG',
-    // 'https://s3-us-west-1.amazonaws.com/carpino-captures/10.JPG',
-    // 'https://s3-us-west-1.amazonaws.com/carpino-captures/11.JPG',
-    // 'https://s3-us-west-1.amazonaws.com/carpino-captures/12.JPG',
-    // 'https://s3-us-west-1.amazonaws.com/carpino-captures/13.JPG',
-    // 'https://s3-us-west-1.amazonaws.com/carpino-captures/14.JPG',
-    // 'https://s3-us-west-1.amazonaws.com/carpino-captures/15.JPG',
-    'src/assets/photos/01_fav.JPG',
-    'src/assets/photos/02_fav.JPG',
-    'src/assets/photos/03_fav.JPG',
-    'src/assets/photos/04_fav.JPG',
-    'src/assets/photos/05_fav.JPG',
-    'src/assets/photos/06_fav.JPG',
-    'src/assets/photos/07_non.JPG',
-    'src/assets/photos/08_non.JPG',
-    'src/assets/photos/09_fav.JPG',
-    'src/assets/photos/10_fav.JPG',
-    'src/assets/photos/11_fav.JPG',
-    'src/assets/photos/12_fav.JPG',
-    'src/assets/photos/13_non.JPG',
-    'src/assets/photos/14_non.JPG',
-    'src/assets/photos/15_fav.JPG',
-    'src/assets/photos/16_fav.JPG',
-    'src/assets/photos/17_fav.JPG'
-  ];
+  imageUrlPrefix = 'https://s3-us-west-1.amazonaws.com/carpino-captures/';
+  favPrefix = 'https://s3-us-west-1.amazonaws.com/carpino-captures/f/small/';
+  nonPrefix = 'https://s3-us-west-1.amazonaws.com/carpino-captures/n/small/';
+  canStillFindImages = true;
+
+  errorMessage = '';
 
   master: string[] = [];
   favorites: string[] = [];
   nonFavorites: string[] = [];
 
-  constructor() {}
+  imageUrls: string[] = [
+    '1_fav.JPG',
+    '2_fav.JPG',
+    '3_fav.JPG',
+    '4_fav.JPG',
+    '5_fav.JPG',
+    '6_fav.JPG',
+    '7_fav.JPG',
+    '8_fav.JPG',
+    '9_fav.JPG',
+    '10_fav.JPG',
+    '11_fav.JPG',
+    '12_fav.JPG',
+    '13_fav.JPG',
+    '1_non.JPG',
+    '2_non.JPG',
+    '3_non.JPG',
+    '4_non.JPG'
+  ];
 
-  populateMasterImageList() {
+  constructor(
+    private http: HttpClient,
+  ) {}
+
+  getImagesFromS3() {
+    const urlPrefix = this.favPrefix;
+    let url = '';
+
+    for (let i = 1; i < 15; ++i) {
+      if (this.canStillFindImages) {
+        url = `${urlPrefix + i.toString()}.JPG`;
+        this.http.get(url, { responseType: 'blob' }).subscribe(
+          () => {
+            console.log('found image');
+            this.master.push(url);
+          },
+          () => {
+            console.log('invalid image');
+            this.canStillFindImages = false;
+          }
+        );
+      }
+    }
+  }
+
+  createFullImageUrls() {
     this.imageUrls.forEach(url => {
       if (url.includes('fav')) {
+        url = this.favPrefix + url;
         this.favorites.push(url);
       } else {
+        url = this.nonPrefix + url;
         this.nonFavorites.push(url);
       }
     });
 
+    this.populateMasterImageList();
+  }
+
+  populateMasterImageList() {
     this.favorites.reverse().forEach(pic => {
       this.master.push(pic);
     });
@@ -67,6 +89,6 @@ export class PortfolioComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.populateMasterImageList();
+    this.createFullImageUrls();
   }
 }
