41026171b1977b63b72b09b27f46c23c53c32111
Vince Carpino <vcarpino33@gmail.com>
2019-06-04 21:41:02 -0700

    Adds bio picture to S3

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -8,6 +8,12 @@ import { Component, OnInit } from '@angular/core';
 export class AboutComponent implements OnInit {
   pageTitle = 'About Me';
 
+  imageUrlPrefix = 'https://s3-us-west-1.amazonaws.com/carpino-captures/';
+
+  getBioImageUrl() {
+    return this.imageUrlPrefix + 'me.JPG';
+  }
+
   constructor() {}
 
   ngOnInit() {}
