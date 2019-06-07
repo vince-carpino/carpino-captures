5d417065e10a07864138864b1d870e0149bb9df7
Vince Carpino <vcarpino33@gmail.com>
2019-06-07 15:32:49 -0700

    Changes structure and styling of about page

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,9 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate
+} from '@angular/animations';
 
 @Component({
   selector: 'cc-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
+  animations: [
+    trigger('fadeIn', [
+      state('in', style({ opacity: 1 })),
+      transition(':enter', [style({ opacity: 0 }), animate(400)])
+    ])
+  ]
 })
 export class AboutComponent implements OnInit {
   pageTitle = 'About Me';
@@ -11,7 +24,7 @@ export class AboutComponent implements OnInit {
   imageUrlPrefix = 'https://s3-us-west-1.amazonaws.com/carpino-captures/';
 
   getBioImageUrl() {
-    return this.imageUrlPrefix + 'me.JPG';
+    return this.imageUrlPrefix + 'me.jpg';
   }
 
   constructor() {}
