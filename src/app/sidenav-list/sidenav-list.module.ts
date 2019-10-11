bc6abfded037840ff49f2f200d6dbdf88075605a
Vince Carpino <carpi111@mail.chapman.edu>
2019-10-11 14:47:54 -0700

    Adds navbar header to sidenav menu

--- src/app/sidenav-list/sidenav-list.module.ts
+++ src/app/sidenav-list/sidenav-list.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { SharedModule } from '../shared/shared.module';
 import { SidenavListComponent } from './sidenav-list.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   imports: [
     MatListModule,
     MatSidenavModule,
+    MatToolbarModule,
     RouterModule,
     SharedModule
   ],
