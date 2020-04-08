f3dc4eaafc33b68f4bdb11597522017f9330b6f9
Vince Carpino <carpi111@mail.chapman.edu>
2020-04-07 19:18:39 -0700

    Removes NgxProgressiveImgLoaderModule from portfolio

--- src/app/portfolio/portfolio.module.ts
+++ src/app/portfolio/portfolio.module.ts
@@ -2,11 +2,9 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { PortfolioComponent } from './portfolio.component';
 import { PortfolioResolver } from '../resolvers/portfolio.resolver';
-// import { NgxProgressiveImgLoaderModule } from 'ngx-progressive-img-loader';
 
 @NgModule({
   imports: [CommonModule],
-  // imports: [CommonModule, NgxProgressiveImgLoaderModule],
   declarations: [PortfolioComponent],
   exports: [PortfolioComponent],
   providers: [PortfolioResolver],
